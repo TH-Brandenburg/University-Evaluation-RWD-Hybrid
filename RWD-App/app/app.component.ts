c0321fd8d7308d3eba24614e6cfe7844dcad9376
Charlene Bertz <charlenebertz@arcor.de>
2016-06-30 14:40:54 +0200

    init angular 2 project
    added project configuration + first files (no compiled files)

---
+++ RWD-App/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Evaluation Technische Hochschule Brandenburg</h1>'
+})
+export class AppComponent { }
