d8f9a1f3c82e1f69ff60cc7d750f9df7ae23c18a
Charlene Bertz <charlenebertz@arcor.de>
2016-07-05 19:23:16 +0200

    added functionality to sender module
    - added check if all multiple choice questions have been answered
    - added navigation to homepage after sending

--- RWD-App/app/sender.component.ts
+++ RWD-App/app/sender.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { QuestionDataService } from './questions.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sender',
   template: `
             <h1>In Sending Process</h1>
+            <button (click)="onSubmit()">Submit</button>
             `
 })
 
 export class SenderComponent {
-  constuctor() {
-
+  private currentSurvey: any;
+  private givenAnswers: any;
+  ngOnInit() {
+    this.currentSurvey = JSON.parse(this.dataService.getQuestionTest());
+    this.currentSurvey = this.currentSurvey.multipleChoiceQuestionDTOs;
+    this.givenAnswers = this.dataService.multipleChoiceAnswers;
+  }
+  constructor(private dataService: QuestionDataService, private router: Router) {
+  }
+  onSubmit() {
+    if (this.givenAnswers < this.currentSurvey.length) {
+      console.log('Zu wenige Antworten gegeben');
+    }
+    else {
+    console.log(this.dataService.textAnswers);
+    //this.dataService.sendAnswers();
+    this.router.navigate(['/']);
+  }
   }
 }
