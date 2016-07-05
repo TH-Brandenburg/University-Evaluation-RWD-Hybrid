8cd7668372aacb0eb689712a78a3c9a1abe0dec2
Charlene Bertz <charlenebertz@arcor.de>
2016-07-05 19:19:58 +0200

    edited navigation
    switched to using count from 1 in case the backend numeration is not
    fitting

--- RWD-App/app/app.component.ts
+++ RWD-App/app/app.component.ts
@@ -10,7 +10,7 @@ import { QuestionDataService, Question, Answer, Survey } from './questions.servi
                 <ul *ngSwitchCase="true">
                   <!--Course and Send Link coded in manually-->
                   <li><a [routerLink]="['/course']" routerLinkActive="active">Choose Course</a></li>
-                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{question.questionID}}</a></li>
+                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{i + 1}}</a></li>
                   <!--insert new question link for every fetched question in init -->
                   <li *ngFor="let question of questions.multipleChoiceQuestionDTOs; let i=index"><a [routerLink]="['/question', i]"> Frage {{i + 1}}</a></li>
                   <li><a [routerLink]="['/send']" routerLinkActive="active">Send</a></li>
@@ -19,7 +19,7 @@ import { QuestionDataService, Question, Answer, Survey } from './questions.servi
                   <li><a [routerLink]="['/course']" routerLinkActive="active">Choose Course</a></li>
                   <!--insert new question link for every fetched question in init -->
                   <li *ngFor="let question of questions.multipleChoiceQuestionDTOs; let i=index"><a [routerLink]="['/question', i]"> Frage {{i + 1}}</a></li>
-                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{question.questionID}}</a></li>
+                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{i + 1}}</a></li>
                   <li><a [routerLink]="['/send']" routerLinkActive="active">Send</a></li>
                 </ul>
               </div>
