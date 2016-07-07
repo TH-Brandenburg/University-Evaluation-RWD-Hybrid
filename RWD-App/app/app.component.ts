214ddc0e8f8dca81d9fd2f0088d0ed3309841551
Charlene Bertz <charlenebertz@arcor.de>
2016-07-07 19:25:18 +0200

    edited navigation
    - put navigation in it's own component
    - added if-rules to app start to prevent showing navigation 2 times in
    main screen
    - not the best solution, but it works for now :/

--- RWD-App/app/app.component.ts
+++ RWD-App/app/app.component.ts
@@ -1,40 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
 import { QuestionDataService, Question, Answer, Survey } from './questions.service';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @Component({
-  selector: 'navigation',
+  selector: 'body',
   template: `
-              <div [ngSwitch]="questions.textQuestionsFirst">
-                <ul *ngSwitchCase="true">
-                  <!--Course and Send Link coded in manually-->
-                  <li><a [routerLink]="['/course']" routerLinkActive="active">Choose Course</a></li>
-                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{i + 1}}</a></li>
-                  <!--insert new question link for every fetched question in init -->
-                  <li *ngFor="let question of questions.multipleChoiceQuestionDTOs; let i=index"><a [routerLink]="['/question', i]"> Frage {{i + 1}}</a></li>
-                  <li><a [routerLink]="['/send']" routerLinkActive="active">Send</a></li>
-                </ul>
-                <ul *ngSwitchDefault>
-                  <li><a [routerLink]="['/course']" routerLinkActive="active">Choose Course</a></li>
-                  <!--insert new question link for every fetched question in init -->
-                  <li *ngFor="let question of questions.multipleChoiceQuestionDTOs; let i=index"><a [routerLink]="['/question', i]"> Frage {{i + 1}}</a></li>
-                  <li *ngFor="let question of questions.textQuestions; let i=index"><a [routerLink]="['/text-question', i]"> Textfrage {{i + 1}}</a></li>
-                  <li><a [routerLink]="['/send']" routerLinkActive="active">Send</a></li>
-                </ul>
-              </div>
-            <router-outlet></router-outlet>
+            <navigation *ngIf="appStarted"></navigation>
+            <button *ngIf="!appStarted" (click)="startApp()">Start App</button>
+            <router-outlet *ngIf="appStarted"></router-outlet>
             `,
-directives: [ROUTER_DIRECTIVES],
+directives: [ROUTER_DIRECTIVES, NavigationComponent],
 providers: [QuestionDataService]
 })
 export class AppComponent implements OnInit {
   //Variable for storing survey data
   questions: any;
+  appStarted: boolean = false;
   ngOnInit() {
   //get survey data on initialization
   this.questions = JSON.parse(this.dataService.getQuestionTest());
   }
-  constructor(private dataService: QuestionDataService){
+  constructor(private dataService: QuestionDataService,
+  private router: Router){
+  }
+  startApp() {
+    this.appStarted = true;
+    this.router.navigate(['/scan']);
   }
 }
---
+++ RWD-App/app/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { QuestionDataService, Question, Answer, Survey } from '../questions.service';
+
+
+@Component({
+  selector: 'navigation',
+  moduleId: module.id,
+  templateUrl: 'navigation.template.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [QuestionDataService]
+})
+export class NavigationComponent implements OnInit {
+  //Variable for storing survey data
+  questions: any;
+  ngOnInit() {
+  //get survey data on initialization
+  this.questions = JSON.parse(this.dataService.getQuestionTest());
+  }
+  constructor(private dataService: QuestionDataService){
+  }
+}
