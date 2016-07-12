208ddaef1b1e916ad2effc8cb7f1d9cf3d62113a
Charlene Bertz <charlenebertz@arcor.de>
2016-07-12 21:17:38 +0200

    small app component edits
    -enabled ngif for demo tomorrow
    -got rid of questions(not needed)

--- RWD-App/app/app.component.ts
+++ RWD-App/app/app.component.ts
@@ -8,26 +8,24 @@ import { NavigationComponent } from './navigation/navigation.component';
   selector: 'body',
   template: `
             <navigation *ngIf="appStarted"></navigation>
-            <img src="" alt="TH Brandenburg Logo" class="startImage" *ngIf="!appStarted" />
+            <img src="app/img/fh-logo.jpg" alt="TH Brandenburg Logo" class="startImage" *ngIf="!appStarted" />
             <button class="startButton" *ngIf="!appStarted" (click)="startApp()">Start App</button>
-            <router-outlet></router-outlet>
+            <router-outlet *ngIf="appStarted"></router-outlet>
             `,
 directives: [ROUTER_DIRECTIVES, NavigationComponent],
 providers: [QuestionDataService]
 })
 export class AppComponent implements OnInit {
   //Variable for storing survey data
-  questions: any;
   appStarted: boolean = false;
   ngOnInit() {
   //get survey data on initialization
-  this.questions = JSON.parse(this.dataService.getQuestionTest());
   }
   constructor(private dataService: QuestionDataService,
   private router: Router){
   }
   startApp() {
     this.appStarted = true;
-    this.router.navigate(['/scan']);
+    //this.router.navigate(['/scan']);
   }
 }
