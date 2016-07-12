e5af1effbccc22f8485f721e1bd6c27238df5a23
Charlene Bertz <charlenebertz@arcor.de>
2016-07-12 21:20:35 +0200

    edited text question component
    more stuff for showing already answeres questons + image preview

--- RWD-App/app/sender/sender.component.ts
+++ RWD-App/app/sender/sender.component.ts
@@ -3,29 +3,30 @@ import { QuestionDataService } from '../questions.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'sender',
-  moduleId: module.id,
-  templateUrl: 'sender.template.html',
+    selector: 'sender',
+    moduleId: module.id,
+    templateUrl: 'sender.template.html',
 })
 
 export class SenderComponent {
-  private currentSurvey: any;
-  private givenAnswers: any;
-  ngOnInit() {
-    this.currentSurvey = JSON.parse(this.dataService.getQuestionTest());
-    this.currentSurvey = this.currentSurvey.multipleChoiceQuestionDTOs.length;
-    this.givenAnswers = this.dataService.getMultipleChoiceAnswersSize() + this.dataService.getTextAnswersSize();
-  }
-  constructor(private dataService: QuestionDataService, private router: Router) {
-  }
-  onSubmit() {
-    if (this.givenAnswers < this.currentSurvey.length) {
-      console.log('Zu wenige Antworten gegeben');
+    private currentSurvey: any;
+    private givenAnswers: any;
+    ngOnInit() {
+        this.currentSurvey = JSON.parse(this.dataService.getQuestionTest());
+        this.currentSurvey = this.currentSurvey.multipleChoiceQuestionDTOs.length;
+        this.givenAnswers = this.dataService.getMultipleChoiceAnswersSize();
     }
-    else {
-    console.log("send answers");
-    this.dataService.sendAnswers();
-    this.router.navigate(['/']);
-  }
-  }
+    constructor(private dataService: QuestionDataService, private router: Router) {
+    }
+    onSubmit() {
+        if (this.givenAnswers < this.currentSurvey) {
+            console.log('Zu wenige Antworten gegeben');
+            alert("Sie haben nicht alle Fragen beantwortet.");
+        }
+        else {
+            console.log("send answers");
+            this.dataService.sendAnswers();
+            this.router.navigate(['/']);
+        }
+    }
 }
--- RWD-App/app/text-question/text-question-showcase.component.ts
+++ RWD-App/app/text-question/text-question-showcase.component.ts
@@ -3,53 +3,56 @@ import { QuestionDataService, Question, Answer, Survey } from '../questions.serv
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'test-question-show',
-  moduleId: module.id,
-  templateUrl: 'text-question.template.html'
+    selector: 'test-question-show',
+    moduleId: module.id,
+    templateUrl: 'text-question.template.html'
 })
 
 export class TextQuestionComponent implements OnInit {
-  private currentQuestion: any;
-  private sub: any;
-  private id: any;
-  private textFirst: any;
-  private textAns: string;
-  private photo: any;
-  private textAnswers: any;
-  ngOnInit() {
-    this.currentQuestion = JSON.parse(this.dataService.getQuestionTest());
-    this.textFirst = this.currentQuestion.textQuestionsFirst;
-    this.currentQuestion = this.currentQuestion.textQuestions;
-    this.textAnswers = this.dataService.textAnswers;
-    this.sub = this.route.params.subscribe(params => {let id = +params['id'];
-    this.id = id;
-    this.textAns = '';
-    this.photo = null;
-  });
-
-  }
-  ngOnDestroy() {
-  this.sub.unsubscribe();
-  }
-
-  constructor(private dataService: QuestionDataService,
-    private route: ActivatedRoute,
-    private router: Router) {
-  }
-  onSubmit() {
-    this.dataService.addTextAnswer(this.currentQuestion[this.id].questionID,this.currentQuestion[this.id].questionText, this.textAns);
-    if (this.photo) {
-      this.dataService.addFile(this.photo);
-    };
-    if (typeof this.currentQuestion[this.id + 1] !== null) {
-      this.router.navigate(['/text-question', this.id + 1]);
-    } else {
-      if (this.textFirst == true) {
-      this.router.navigate(['/question', 0])
-      }
-      else {
-        this.router.navigate(['/send'])
-      }
+    private fetchedQuestions: any;
+    private sub: any;
+    private id: any;
+    private textFirst: any;
+    private textAns: string;
+    private photo: any;
+    private givenTextAnswers: any[];
+    private givenImages: any[];
+    ngOnInit() {
+        this.fetchedQuestions = JSON.parse(this.dataService.getQuestionTest());
+        this.textFirst = this.fetchedQuestions.textQuestionsFirst;
+        this.fetchedQuestions = this.fetchedQuestions.textQuestions;
+        this.givenTextAnswers = this.dataService.getTextAnswers();
+        this.sub = this.route.params.subscribe(params => {
+            let id = +params['id'];
+            this.id = id;
+        });
+        this.givenImages = this.dataService.getImages();
+        this.textAns = '';
+        this.photo = null;
     }
-  }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    constructor(private dataService: QuestionDataService,
+        private route: ActivatedRoute,
+        private router: Router) {
+    }
+    onSubmit(value: string) {
+        this.dataService.addTextAnswer(this.fetchedQuestions[this.id].questionID, this.fetchedQuestions[this.id].questionText, value);
+        if (this.photo) {
+            this.dataService.addImageAnswer(this.photo);
+        };
+        if ((this.id + 1) <= this.dataService.getMultipleChoiceAnswersSize()) {
+            this.router.navigate(['/text-question', this.id + 1]);
+        } else {
+            if (this.textFirst == true) {
+                this.router.navigate(['/question', 0])
+            }
+            else {
+                this.router.navigate(['/send'])
+            }
+        }
+    }
 }
