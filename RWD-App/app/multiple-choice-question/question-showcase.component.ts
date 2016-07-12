585dc269c0404a03175327e4169db04f279f8aa6
Charlene Bertz <charlenebertz@arcor.de>
2016-07-12 21:18:59 +0200

    edited question show
    preperation for showing already answeres questions

--- RWD-App/app/multiple-choice-question/question-showcase.component.ts
+++ RWD-App/app/multiple-choice-question/question-showcase.component.ts
@@ -3,44 +3,54 @@ import { QuestionDataService, Question, Answer, Survey } from '../questions.serv
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'question-show',
-  moduleId: module.id,
-  templateUrl: 'question-showcase.template.html'
+    selector: 'question-show',
+    moduleId: module.id,
+    templateUrl: 'question-showcase.template.html'
 })
 
 export class QuestionComponent implements OnInit {
-  currentQuestion: any;
-  private sub: any;
-  private id: any;
-  private textFirst: boolean;
-  ngOnInit() {
-    this.currentQuestion = JSON.parse(this.dataService.getQuestionTest());
-    this.textFirst = this.currentQuestion.textQuestionsFirst;
-    this.currentQuestion = this.currentQuestion.multipleChoiceQuestionDTOs;
-    this.sub = this.route.params.subscribe(params => {let id = +params['id'];
-    this.id = id;
-  });
+    private currentQuestion: any;
+    private sub: any;
+    private id: any;
+    private textFirst: boolean;
+    private givenMCAnswers: any[];
+    private chosenChoice: any[];
+    ngOnInit() {
+        this.currentQuestion = JSON.parse(this.dataService.getQuestionTest());
+        this.textFirst = this.currentQuestion.textQuestionsFirst;
+        this.currentQuestion = this.currentQuestion.multipleChoiceQuestionDTOs;
+        this.sub = this.route.params.subscribe(params => {
+            let id = +params['id'];
+            this.id = id;
+            this.givenMCAnswers = this.dataService.getMultipleChoiceAnswers();
+        });
 
-  }
-  //needed to erase observer after leaving question area
-  ngOnDestroy() {
-  this.sub.unsubscribe();
-  }
+        for (var givenAnswer of this.givenMCAnswers) {
+            if (givenAnswer.questionText == this.currentQuestion[this.id].questionText) {
+                this.chosenChoice = givenAnswer.choice;
+            }
+        }
 
-  constructor(private dataService: QuestionDataService,
-    private route: ActivatedRoute,
-    private router: Router) {
-  }
-  onClickAnswer(answer: any) {
-    this.dataService.addMultipleChoiceAnswer(this.currentQuestion[this.id].question, answer.choiceText, answer.grade);
-    if (this.id + 1 > this.currentQuestion.length) {
-      if (this.textFirst == true) {
-      this.router.navigate(['/send'])
-      }
-      else {
-        this.router.navigate(['/text-question',0])
-      }
     }
-    else this.router.navigate(['/question', this.id + 1]);
-  }
+    //needed to erase observer after leaving question area
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    constructor(private dataService: QuestionDataService,
+        private route: ActivatedRoute,
+        private router: Router) {
+    }
+    onClickAnswer(answer: any) {
+        this.dataService.addMultipleChoiceAnswer(this.currentQuestion[this.id].question, answer.choiceText, answer.grade);
+        if (this.id + 1 > this.currentQuestion.length) {
+            if (this.textFirst == true) {
+                this.router.navigate(['/send'])
+            }
+            else {
+                this.router.navigate(['/text-question', 0])
+            }
+        }
+        else this.router.navigate(['/question', this.id + 1]);
+    }
 }
