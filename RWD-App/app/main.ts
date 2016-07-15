b9e371e11a7a4e8504045f714a6927d4a51fa9fe
Charlene Bertz <charlenebertz@arcor.de>
2016-07-15 22:35:44 +0200

    edited text question functionality
    added formbuilder form to fix pushing text to answers

--- RWD-App/app/main.ts
+++ RWD-App/app/main.ts
@@ -3,7 +3,8 @@ import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {JSONP_PROVIDERS} from '@angular/http';
+import { provideForms } from '@angular/forms';
 
 bootstrap(AppComponent, [
-  APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, JSONP_PROVIDERS
+  APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, JSONP_PROVIDERS, provideForms() 
 ]).catch((err: any) => console.error(err));
--- RWD-App/app/text-question/text-question-showcase.component.ts
+++ RWD-App/app/text-question/text-question-showcase.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { QuestionDataService, Question, Answer, Survey } from '../questions.service';
+import { Component, OnInit } from '@angular/core';
+import {  FORM_DIRECTIVES,  REACTIVE_FORM_DIRECTIVES,  FormBuilder,  FormGroup  } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { QuestionDataService, Question, Answer, Survey } from '../questions.service';
 
 @Component({
     selector: 'test-question-show',
     moduleId: module.id,
-    templateUrl: 'text-question.template.html'
+    templateUrl: 'text-question.template.html',
+    directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
 })
 
 export class TextQuestionComponent implements OnInit {
@@ -13,21 +15,28 @@ export class TextQuestionComponent implements OnInit {
     private sub: any;
     private id: any;
     private textFirst: any;
-    private textAns: string;
-    private photo: any;
     private givenTextAnswers: any[];
-    private givenImages: any[];
+    private givenImages: File[];
+    private answerText: string = '';
+    private answerImage: File;
+    private answerForm: FormGroup;
+    private alreadyAnswered: boolean = false;
+
     ngOnInit() {
         this.fetchedQuestions = JSON.parse(this.dataService.getQuestionTest());
         this.textFirst = this.fetchedQuestions.textQuestionsFirst;
         this.fetchedQuestions = this.fetchedQuestions.textQuestions;
         this.givenTextAnswers = this.dataService.getTextAnswers();
+        for (var givenAnswer of this.givenTextAnswers) {
+          if (givenAnswer['questionID'] === this.fetchedQuestions[this.id].questionID) {
+            this.alreadyAnswered = true;
+            this.answerText = givenAnswer.answerText;
+          }
+        }
         this.sub = this.route.params.subscribe(params => {let id = +params['id'];
             this.id = id;
         });
         this.givenImages = this.dataService.getImages();
-        this.textAns = '';
-        this.photo = null;
     }
 
     ngOnDestroy() {
@@ -36,12 +45,20 @@ export class TextQuestionComponent implements OnInit {
 
     constructor(private dataService: QuestionDataService,
         private route: ActivatedRoute,
-        private router: Router) {
+        private router: Router, fb: FormBuilder) {
+          this.answerForm = fb.group({
+            'text': [this.answerText],
+            'image': ['']
+          });
     }
+
     onSubmit(value: string) {
-        this.dataService.addTextAnswer(this.fetchedQuestions[this.id].questionID, this.fetchedQuestions[this.id].questionText, value);
-        if (this.photo) {
-            this.dataService.addImageAnswer(this.photo);
+        if (!this.alreadyAnswered) {this.dataService.addTextAnswer(this.fetchedQuestions[this.id].questionID, this.fetchedQuestions[this.id].questionText, value['text']);}
+        console.log('Image: ' + value['image']);
+        console.log(this.dataService.getMultipleChoiceAnswersSize());
+        if (value['image']) {
+            //this.dataService.addImageAnswer(value['image']);
+            console.log(value['image']);
         };
         if ((this.id + 1) < this.dataService.getMultipleChoiceAnswersSize()) {
             this.router.navigate(['/text-question', this.id + 1]);
