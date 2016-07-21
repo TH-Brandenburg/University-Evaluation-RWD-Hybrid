c7956d719a87d5e65df41e1e687afb1d86c3d180
Charlene Bertz <charlenebertz@arcor.de>
2016-07-21 13:15:01 +0200

    added manual focusing of navigation
    - added manual focus set on navigation

--- RWD-App/app/navigation/navigation.component.ts
+++ RWD-App/app/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ROUTER_DIRECTIVES, ActivatedRoute, Router } from '@angular/router';
 import { QuestionDataService, Question, Answer, Survey } from '../questions.service';
 
 
@@ -9,13 +9,45 @@ import { QuestionDataService, Question, Answer, Survey } from '../questions.serv
   templateUrl: 'navigation.template.html',
   directives: [ROUTER_DIRECTIVES]
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, AfterViewInit {
   //Variable for storing survey data
-  questions: any;
+  private questions: any;
+  private subID: any;
+  private id: number;
+  private component: any;
   ngOnInit() {
-  //get survey data on initialization
   this.questions = JSON.parse(this.dataService.getQuestionTest());
+  this.subID = this.activeRoute.params.subscribe(params => {let id = +params['id'];
+      this.id = id;
+      //this.component = component;
+  });
+  //var subComponent = this.activeRoute.data.subscribe(url => {console.log(url)});
+  this.component = this.activeRoute.snapshot.url[0].path;
+  console.log(this.component);
   }
-  constructor(private dataService: QuestionDataService){
+
+  ngAfterViewInit() {
+    switch(this.component)
+    {
+      case 'course':
+        document.getElementById("course").focus();
+        break;
+      case 'question':
+        document.getElementById("mc-" + this.id).focus();
+        break;
+      case 'text-question':
+        document.getElementById("text-" + this.id).focus();
+        break;
+      case 'send':
+        document.getElementById("send").focus();
+        break;
+      default:
+        break;
+    }
+  }
+
+  constructor(private dataService: QuestionDataService,
+  private activeRoute: ActivatedRoute,
+  private route: Router){
   }
 }
