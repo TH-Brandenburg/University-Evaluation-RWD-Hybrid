c13cd04c7a29bfadf48b2b73e5670ae2faef46cd
MSevener <maxireg@web.de>
2016-08-05 10:48:18 +0200

    qrscanner not mirrored

--- RWD-App/app/scanner/js/qr-scanner.js
+++ RWD-App/app/scanner/js/qr-scanner.js
@@ -20,7 +20,6 @@ angular.module('qrScanner', ["ng"]).directive('qrScanner', ['$interval', '$windo
       var video = $window.document.createElement('video');
       video.setAttribute('width', width);
       video.setAttribute('height', height);
-      video.setAttribute('style', '-moz-transform:rotateY(-180deg);-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);');
       var canvas = $window.document.createElement('canvas');
       canvas.setAttribute('id', 'qr-canvas');
       canvas.setAttribute('width', width);
